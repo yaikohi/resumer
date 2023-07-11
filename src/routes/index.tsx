82574692c46f3ed336c61b406065fb87ff27bd94
yaikohi <erik.beem@hotmail.com>
2023-07-11 10:59:47 +0200

    wip: trying to fix the ui update but its not working yet

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,18 +19,6 @@ const resumeSchema = z.object({
 
 type TResumeForm = z.infer<typeof resumeSchema>;
 
-export const useGetResumes = routeLoader$(async () => {
-  return await getPublicResumes();
-});
-
-export const usePostResumeLoader = routeLoader$<InitialValues<TResumeForm>>(
-  () => {
-    return ({
-      content: "",
-    });
-  },
-);
-
 export const usePostResumeAction = formAction$<TResumeForm>(
   async (data, requestEvent) => {
     const session = requestEvent.sharedMap.get("session");
@@ -44,8 +32,20 @@ export const usePostResumeAction = formAction$<TResumeForm>(
       content: data.content,
     });
   },
-  zodForm$(resumeSchema),
+  zodForm$(resumeSchema)
 );
+
+export const usePostResumeLoader = routeLoader$<InitialValues<TResumeForm>>(
+  () => {
+    return {
+      content: "",
+    };
+  }
+);
+
+export const useGetResumes = routeLoader$(async () => {
+  return await getPublicResumes();
+});
 
 export default component$(() => {
   const session = useAuthSession();
@@ -61,78 +61,79 @@ export default component$(() => {
     <>
       <h1 class="text-3xl">Resumer</h1>
       <p>Send resumes on resumer, a qwik twitter.</p>
-      {/* User profile section? */}
-      {session.value?.user?.name && (
-        <div class="my-8">
-          <div class="flex place-items-center gap-2">
-            {session.value.user.image && (
-              <img
-                width={64}
-                height={64}
-                class="rounded-full"
-                src={session.value.user.image}
-                alt={`${session.value.user.name} picture`}
-              />
-            )}
-            <p>{session.value.user.name}</p>
-          </div>
-        </div>
-      )}
-      {/* Creating a new resume!*/}
-      {session.value?.user?.name && (
-        <div>
-          <Form class="flex flex-col place-items-center gap-2">
-            <Field name="content">
-              {(field, props) => {
-                // TEST IF THIS ALSO WORKS IN PROD!
-                // this clears the form after submitting.
-                field.value = "";
-                return (
-                  <>
-                    <PostResumeTextArea
-                      {...props}
-                      name="content"
-                      value={field.value}
-                      error={field.error}
-                    />
-                  </>
-                );
-              }}
-            </Field>
-            <div class="flex flex-row relative place-items-center w-full justify-end ">
-              <div class="absolute -top-2 left-0">
-                {postResumeform.submitting && (
-                  <>
-                    <p class="text-sm italic">loading...</p>
-                  </>
-                )}
-
-                {postResumeform.submitCount >= 200 && (
-                  <>
-                    <p class="text-sm text-destructive">
-                      That's too many characters.
-                    </p>
-                  </>
-                )}
-              </div>
-              <button
-                disabled={postResumeform.submitting}
-                class={cn(
-                  "inline-flex items-center justify-center ",
-                  "px-4 py-3 rounded-3xl",
-                  "bg-primary ring-offset-background ",
-                  "text-sm text-primary-foreground font-medium",
-                  "transition-colors",
-                  "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-                  "disabled:pointer-events-none disabled:opacity-50",
-                )}
-              >
-                Send resume
-              </button>
+      <div class={cn("flex", "w-full", "p-2")}>
+        {/* User profile section? */}
+        {session.value?.user?.name && (
+          <div class="my-8">
+            <div class="flex place-items-center gap-2">
+              {session.value.user.image && (
+                <img
+                  width={48}
+                  height={48}
+                  class="aspect-square rounded-full"
+                  src={session.value.user.image}
+                  alt={`${session.value.user.name} picture`}
+                />
+              )}
             </div>
-          </Form>
-        </div>
-      )}
+          </div>
+        )}
+        {/* Creating a new resume!*/}
+        {session.value?.user?.name && (
+          <div class={cn("w-full")}>
+            <Form class="flex flex-col place-items-center gap-2">
+              <Field name="content">
+                {(field, props) => {
+                  // TEST IF THIS ALSO WORKS IN PROD!
+                  // this clears the form after submitting.
+                  field.value = "";
+                  return (
+                    <>
+                      <PostResumeTextArea
+                        {...props}
+                        name="content"
+                        value={field.value}
+                        error={field.error}
+                      />
+                    </>
+                  );
+                }}
+              </Field>
+              <div class="flex flex-row relative place-items-center w-full justify-end ">
+                <div class="absolute -top-2 left-0">
+                  {postResumeform.submitting && (
+                    <>
+                      <p class="text-sm italic">loading...</p>
+                    </>
+                  )}
+
+                  {postResumeform.submitCount >= 200 && (
+                    <>
+                      <p class="text-sm text-destructive">
+                        That's too many characters.
+                      </p>
+                    </>
+                  )}
+                </div>
+                <button
+                  disabled={postResumeform.submitting}
+                  class={cn(
+                    "inline-flex items-center justify-center ",
+                    "px-4 py-3 rounded-3xl",
+                    "bg-primary ring-offset-background ",
+                    "text-sm text-primary-foreground font-medium",
+                    "transition-colors",
+                    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+                    "disabled:pointer-events-none disabled:opacity-50"
+                  )}
+                >
+                  Send resume
+                </button>
+              </div>
+            </Form>
+          </div>
+        )}
+      </div>
       {/* Container of list of resumes*/}
       <div class="my-8">
         {/* List of resumes */}
